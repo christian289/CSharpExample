cba541006c2308f49ee4b25c49a33849fbb15d6f
christian289 <christian289@naver.com>
2022-05-19 11:22:40 +0900

    Update StaticConstructorTest.cs

--- CSharpExample/CsharpExampleTest/StaticConstructorTest.cs
+++ CSharpExample/CsharpExampleTest/StaticConstructorTest.cs
@@ -31,4 +31,8 @@ namespace CsharpExampleTest
             TestString = "정적 생성자의 호출 타이밍 확인하시오";
         }
     }
+
+    // https://stackoverflow.com/questions/7400438/why-this-static-constructor-is-not-getting-called
+    // 위 문서에 따르면 const일 경우 정적 생성자가 호출되지 않는다.
+    // 그런데 어찌보면 당연하다. const는 컴파일하면서 모두 코드상에 초기화하는 값으로 모두 변환되고 컴파일 된다. 인스턴스라는게 애초에 존재하지 않고 value만 있는 게 const다.
 }
