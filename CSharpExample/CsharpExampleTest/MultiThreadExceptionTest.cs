770da7a5a742cb46b075bab054f1f37d780ebee8
christian289 <christian289@naver.com>
2023-02-20 19:15:55 +0900

    Create MultiThreadExceptionTest.cs
    멀티스레딩할 때 Exception을 밖에서 잡을 지 안에서 잡을지 현상을 알아본다.
    너무나 당연한 얘기겠지만, 안에서 잡아야 다른 스레드들은 영향을 받지 않고 돈다.

---
+++ CSharpExample/CsharpExampleTest/MultiThreadExceptionTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CsharpExampleTest
+{
+    public class MultiThreadExceptionTest
+    {
+        private readonly ITestOutputHelper output;
+
+        public MultiThreadExceptionTest(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public async void Test1()
+        {
+            List<Task> tasks = new();
+
+            try
+            {
+                foreach (int item in Enumerable.Range(1, 10))
+                {
+                    tasks.Add(Task.Run(() =>
+                    {
+                        if (item == 1)
+                        {
+                            throw new Exception("1 Exception");
+                        }
+
+                        output.WriteLine(item.ToString());
+                    }));
+                }
+            }
+            catch (Exception ex)
+            {
+                output.WriteLine(ex.Message);
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        [Fact]
+        public async void Test2()
+        {
+            List<Task> tasks = new();
+
+            foreach (int item in Enumerable.Range(1, 10))
+            {
+                tasks.Add(Task.Run(() =>
+                {
+                    try
+                    {
+                        if (item == 1)
+                        {
+                            throw new Exception("1 Exception");
+                        }
+
+                        output.WriteLine(item.ToString());
+                    }
+                    catch (Exception ex)
+                    {
+                        output.WriteLine(ex.Message);
+                    }
+                }));
+            }
+
+            await Task.WhenAll(tasks);
+        }
+    }
+}
