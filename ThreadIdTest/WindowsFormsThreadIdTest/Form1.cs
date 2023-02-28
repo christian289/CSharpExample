95a3af14a5b212220c3271a17257145b3d4ce5e1
christian289 <christian289@naver.com>
2023-02-28 19:38:46 +0900

    Update Form1.cs

--- ThreadIdTest/WindowsFormsThreadIdTest/Form1.cs
+++ ThreadIdTest/WindowsFormsThreadIdTest/Form1.cs
@@ -1,4 +1,4 @@
-using System.Diagnostics;
+ï»¿using System.Diagnostics;
 
 namespace WindowsFormsThreadIdTest
 {
@@ -7,18 +7,59 @@ namespace WindowsFormsThreadIdTest
         public Form1()
         {
             InitializeComponent();
-            Button button = new();
-            button.Click += button1_Click;
-            this.Controls.Add(button);
-        }
 
-        private async void button1_Click(object? sender, EventArgs e)
-        {
-            Debug.WriteLine($"Click ¸Þ¼­µå ÁøÀÔ: {Environment.CurrentManagedThreadId}");
+            Button button1 = new()
+            {
+                Text = "Default",
+                Dock = DockStyle.Fill
+            };
+            button1.Click += async (sender, e) =>
+            {
+                Debug.WriteLine("button1 ì‹œìž‘");
+                Debug.WriteLine($"Thread Id: {Environment.CurrentManagedThreadId}");
+                await Task.Run(() => Debug.WriteLine($"Thread Id: {Environment.CurrentManagedThreadId}"));
+                Debug.WriteLine($"Thread Id: {Environment.CurrentManagedThreadId}");
+            };
 
-            await Task.Run(() => Debug.WriteLine($"Inner: {Environment.CurrentManagedThreadId}"));
+            Button button2 = new()
+            {
+                Text = "ConfigureAwait(true)",
+                Dock = DockStyle.Fill
+            };
+            button2.Click += async (sender, e) =>
+            {
+                Debug.WriteLine("button2 ì‹œìž‘");
+                Debug.WriteLine($"Thread Id: {Environment.CurrentManagedThreadId}");
+                await Task.Run(() => Debug.WriteLine($"Thread Id: {Environment.CurrentManagedThreadId}")).ConfigureAwait(true);
+                Debug.WriteLine($"Thread Id: {Environment.CurrentManagedThreadId}");
+            };
 
-            Debug.WriteLine($"Click ¸Þ¼­µå Á¾·á: {Environment.CurrentManagedThreadId}");
+            Button button3 = new()
+            {
+                Text = "ConfigureAwait(false)",
+                Dock = DockStyle.Fill
+            };
+            button3.Click += async (sender, e) =>
+            {
+                Debug.WriteLine("button3 ì‹œìž‘");
+                Debug.WriteLine($"Thread Id: {Environment.CurrentManagedThreadId}");
+                await Task.Run(() => Debug.WriteLine($"Thread Id: {Environment.CurrentManagedThreadId}")).ConfigureAwait(false);
+                Debug.WriteLine($"Thread Id: {Environment.CurrentManagedThreadId}");
+            };
+
+            TableLayoutPanel tableLayoutPanel = new();
+            tableLayoutPanel.SuspendLayout();
+            tableLayoutPanel.RowCount = 1;
+            tableLayoutPanel.ColumnCount = 3;
+            tableLayoutPanel.Dock = DockStyle.Fill;
+            tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 200));
+            tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 200));
+            tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 200));
+            tableLayoutPanel.Controls.Add(button1, 0, 0);
+            tableLayoutPanel.Controls.Add(button2, 1, 0);
+            tableLayoutPanel.Controls.Add(button3, 2, 0);
+            tableLayoutPanel.ResumeLayout();
+            this.Controls.Add(tableLayoutPanel);
         }
     }
 }
